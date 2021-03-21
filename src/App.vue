07e8a06280904dad72f90e8558001858626cf4a6
Delectabecca <71186556+Delectabecca@users.noreply.github.com>
2021-03-21 15:48:56 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div @click="show()" style="display:inline">Show</div> <div @click="hide()" style="display:inline">Hide</div>
+    <div id="actions" :class="{ hidden: curMenu!='actions'}">
+    <div class="row" style="width:100%">
+      <div class="col-sm-4">
+        This is a test
+      </div>
+      <div class="col-sm-8">
+        <div id="actions" class="row overflow-auto" style="height:100px">
+          <div style="padding:10px" :class="{ 'col-sm-6': true, hidden: action.unlocked != 1 }" v-for="action in actions" :key="action.name">
+              <div class="rounded" style="background-color:grey;border-color:black;border-width:2px;border-style:solid;color:white;"><center>{{action.name}}</center></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default{
+  data() {
+    return{
+      actions: {
+        eat: {
+          name: "Eat",
+          unlocked: 1
+          },
+        sleep: {
+          name: "Sleep",
+          unlocked: 1
+        },
+        lift: {
+          name: "Lift Weights"
+        },
+        run: {
+          name: "Run"
+        },
+        magic: {
+          name: "Do Magic"
+        }
+      },
+      curMenu: String
+    }
+  },
+  methods: {
+    show: function() {
+      this.curMenu = "actions"
+    },
+    hide: function() {
+      this.curMenu = ""
+    }
+  }
+}
+
+</script>
+
+<style>
+.hidden {
+  display:none;
+}
+</style>
